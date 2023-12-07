6d9435ae481b22a6c4d275ad46772fd03836377c
Pratibha Bista <pratibha@Pratibhas-MacBook-Air.local>
2023-12-07 21:18:06 +0545

    added test for greatest num

---
+++ greatest_num_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'greatest_num'
+
+RSpec.describe do
+  describe "#get_user_input" do
+		it "asks input from the user with respective position" do
+			allow_any_instance_of(Kernel).to receive(:puts)
+
+			result = get_user_input("first")
+
+			allow_any_instance_of(Kernel).to receive(:puts).and_return("Enter first number: ")
+		end
+  end
+
+  describe "#greatest_among_three" do
+    it "returns greatest among three numbers" do
+			num1 = 20
+			num2 = 70
+			num3 = 40
+
+      result = greatest_among_three(num1, num2, num3)
+      expect(result).to eq(70)
+    end
+  end
+end
